f33ec0d01367cd03b85d18c9005ea53c080f1c86
Noah S <noah@noahjs.com>
2014-04-23 09:34:57 -0700

    Use larval seeder instead

--- laravel/app/controllers/HomeController.php
+++ laravel/app/controllers/HomeController.php
@@ -17,43 +17,6 @@ class HomeController extends BaseController {
 
 	public function seed()
 	{
-    $faker = Faker\Factory::create();
-    $authors = [];
-
-    for( $i=0; $i< 5000; $i++ ){
-
-
-      $author = new Author();
-      $author->first_name	=	$faker->firstName;
-      $author->middle_name  = $faker->firstName;
-      $author->last_name  =	$faker->lastName;
-
-      $author->phone  =	'1'.mt_rand(2222222222,7999999999);
-      $author->birthdate  =	$faker->dateTimeBetween("-49 years", "-18 years")->format("Y-m-d");
-
-      $author->address  =	$faker->streetAddress;
-      $author->address2 =	$faker->secondaryAddress;
-      $author->city =	$faker->city;
-      $author->state  =	$faker->state;
-      $author->zipcode  =	$faker->postcode;
-      $author->countrycode=	"US";
-
-      $author->save();
-    }
-
-    for( $i=0; $i< 100000; $i++ ){
-
-
-      $post = new Post();
-      $post->author_id	=	mt_rand(1,5000);
-
-      $post->title    = $faker->realText(mt_rand(75,250));
-      $post->content  = $faker->realText(mt_rand(400,5000));
-
-      $post->save();
-
-    }
-
 		return 'Done';
 	}
 
